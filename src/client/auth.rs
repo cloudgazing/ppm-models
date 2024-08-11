3d0a7ec878a1da376338bf62c83cc0897b34063b
cloudgazing <cloudmiles00@gmail.com>
2024-08-11 20:33:04 +0300

    Added lifetimes to client auth

--- src/client/auth.rs
+++ src/client/auth.rs
@@ -1,20 +1,37 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct LoginData {
-	pub username: String,
-	pub password: String,
+pub struct LoginData<'a> {
+	pub username: &'a str,
+	pub password: &'a str,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct SignupData {
-	pub username: String,
-	pub password: String,
-	pub display_name: String,
+pub struct SignupData<'a> {
+	pub username: &'a str,
+	pub password: &'a str,
+	pub display_name: &'a str,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-pub enum Auth {
-	Login(LoginData),
-	Signup(LoginData),
+pub enum Auth<'a> {
+	#[serde(borrow)]
+	Login(LoginData<'a>),
+	Signup(SignupData<'a>),
+}
+
+impl<'a> LoginData<'a> {
+	pub fn new(username: &'a str, password: &'a str) -> Self {
+		Self { username, password }
+	}
+}
+
+impl<'a> SignupData<'a> {
+	pub fn new(username: &'a str, password: &'a str, display_name: &'a str) -> Self {
+		Self {
+			username,
+			password,
+			display_name,
+		}
+	}
 }
